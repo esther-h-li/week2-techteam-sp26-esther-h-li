f420ff8793276b7f7e05255211797873207575ee
esther-h-li <estherhuanneli@gmail.com>
2026-02-26 16:35:35 -0500

    Completed

--- app/main.py
+++ app/main.py
@@ -15,53 +15,54 @@ def read_root():
 
 @app.get("/users")
 def read_users():
-    # TODO: return all users from the database
-    pass
+    return db_users
 
 
 @app.post("/user")
-def create_user():
-    # TODO: create a new user in the database
-    pass
+def create_user(user: User):
+    db_users[user.id] = user
 
 
 @app.put("/user")
-def update_user():
-    # TODO: update an existing user in the database
-    pass
+def update_user(user: User):
+    if user.id in db_users:
+        db_users[user.id] = user
 
 
 @app.get("/user")
 def get_all_users_prefix(prefix: str):
-    # TODO: return all users from the database that match the given name prefix
-    pass
+    users = []
+    for user in db_users:
+        if(user.name.startsWith(prefix)):
+            users.append(user)
+    return users
+
+ 
 
 
 @app.delete("/user")
-def delete_user():
-    # TODO: delete an existing user from the database
-    pass
+def delete_user(user: User):
+    if user.id in db_users:
+        db_users.remove(user.id)
 
 
 @app.get("/products")
 def read_products():
-    # TODO: return all products from the database
-    pass
+    return db_products
 
 
 @app.post("/product")
-def create_product():
-    # TODO: create a new product in the database
-    pass
+def create_product(product: Product):
+    db_products[product.id] = product
 
 
 @app.put("/product")
-def update_product():
-    # TODO: update an existing product in the database
-    pass
+def update_product(product: Product):
+    if(product.id in db_products):
+        db_products[product.id] = product
 
 
 @app.delete("/product")
-def delete_product():
-    # TODO: delete an existing product from the database
-    pass
+def delete_product(product: Product):
+        if(product.id in db_products):
+            del db_products[product.id]
